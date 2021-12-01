d54a77a2be455e4b74d8efb0680fe64238f7419d
Arman Borhan <91340628+Fennec-3@users.noreply.github.com>
2021-12-01 12:10:38 -0600

    started shop
    need to add items to shop

--- sketch.js
+++ sketch.js
@@ -10,20 +10,29 @@
 let cookie, shop, upgrade;
 let cookieImg, clickedCookie, shopImg;
 let cookieCounter = 0;
+let minHW, shopHeight, shopWidth;
+let isShop = false;
 
 function preload() {
   cookieImg = loadImage("assets/Cookie.png");
   clickedCookie = loadImage("assets/Cookie.png");
   shopImg = loadImage("assets/Shop.png");
+  clickedShop = loadImage("assets/Shop.png");
 }
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  cookieImg.resizeNN(min(height, width)/3, min(height, width)/3);
-  clickedCookie.resizeNN(min(height, width)/3-10, min(height, width)/3-10);
+  minHW = min(height, width);
+  shopWidth = width/5;
+  shopHeight = height/1.5;
+
+  cookieImg.resizeNN(minHW/3, minHW/3);
+  clickedCookie.resizeNN(minHW/3-10, minHW/3-10);
+  shopImg.resizeNN(minHW/8, minHW/8);
+  clickedShop.resizeNN(minHW/8-10, minHW/8-10);
 
   cookie = new Button(width/2, height/2, cookieImg, clickedCookie);
-  shop = new Button(width-50, 50, shopImg);
+  shop = new Button(width-50, 50, shopImg, clickedShop);
   // upgrade = new Button(width-50, 160);
 
 }
@@ -35,6 +44,7 @@ function draw() {
   // upgrade.display();
 
   displayText(width/2, cookie.y-cookie.radius*1.5, "Cookies: " + cookieCounter, min(height, width)/14);
+  showShop();
 }
 
 class Button {
@@ -76,11 +86,25 @@ function mousePressed() {
     cookieCounter++;
     cookie.buttonPressed();
   }
+  if (shop.mouseDetected()) {
+    shop.buttonPressed();
+    isShop = !isShop;
+  }
 }
 
 function displayText(x, y, words, sizeOfText) {
   fill("white");
+  strokeWeight(0);
   textAlign(CENTER, CENTER);
   textSize(sizeOfText);
   text(words, x, y);
+}
+
+function showShop() {
+  if (isShop === true) {
+    strokeWeight(5);
+    stroke(220);
+    rect(10, 10, shopWidth, shopHeight);
+  }
+
 }
